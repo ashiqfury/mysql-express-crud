f5901de063b10f28b398ea6bc5a933091f801cf0
Fury <ashiqasraf07@gmail.com>
2022-01-21 15:00:40 +0530

    Added a sql file

---
+++ rest.sql
@@ -0,0 +1,17 @@
+USE nodemysql;
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  age INT NOT NULL,
+  mark INT NOT NULL
+);
+
+INSERT INTO users (name, age, mark) VALUES('fury', 20, 89);
+UPDATE users SET mark=99 WHERE id=8;
+
+SELECT * FROM users;
+
+
+-- 2 ways to run sql file in terminal
+-- mysql> source ./filename.sql
+-- mysql -u root -p dbname < ./filename.sql
